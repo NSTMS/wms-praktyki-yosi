eb62994d2446c4bdb604b2299d3ff1e68c8214e7
NSTMS <jan.idzi1@gmail.com>
2023-03-15 14:10:31 +0100

    add comment to constructor

--- wms-praktyki-yosi-frontend/wms-frontend/src/app/data-reader.service.ts
+++ wms-praktyki-yosi-frontend/wms-frontend/src/app/data-reader.service.ts
@@ -6,33 +6,7 @@ import { type product, type productToAdd } from './types/productTypes';
 export class DataReaderService {
   link : string = 'http://189.91.31.162:5000/api/products'
   columns = ["id", "productName", "ean", "price", "quantity"]
-  constructor() {
-    // this.data = [
-    //   {
-    //     id: 0,
-    //     productName: "Apples",
-    //     EAN: "3748567323872",
-    //     price: 12,
-    //     quantity: 2879
-    //   },
-    //   {
-    //     id: 1,
-    //     productName: "Pears",
-    //     EAN: "1874398798572",
-    //     price: 6.80,
-    //     quantity: 1783
-    //   },
-    //   {
-    //     id: 2,
-    //     productName: "Oranges",
-    //     EAN: "98137598757932",
-    //     price: 7.20,
-    //     quantity: 3012
-    //   }
-    // ];
-
-
-  }
+  // constructor() {}
 
 
   async GetAll() {
