4931a01a842e59d4fb6e6c23b43b225ea5a740fc
cornHarvester <jan.socha.szkolny@gmail.com>
2023-03-22 09:33:31 +0100

    broke down `app.module.ts` into smaller files

---
+++ wms-praktyki-yosi-frontend/wms-frontend/src/app/Modules/compomponents.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddFormComponent } from '@components/product-pages/add-form/add-form.component'
+import { LoginFormComponent } from '@components/user-pages/login-form/login-form.component';
+import { RegisterFormComponent } from '@components/user-pages/register-form/register-form.component';
+import { UsersTableComponent } from '@components/user-pages/users-table/users-table.component';
+import { UsersEditComponent } from '@components/user-pages/users-edit/users-edit.component';
+import { ProductInfoComponent } from '@components/product-pages/product-info/product-info.component';
+import { EditLocationComponent } from '@components/location-pages/edit-location/edit-location.component';
+import { AddLocationComponent } from '@components/location-pages/add-location/add-location.component';
+import { DataTableComponent } from '@components/product-pages/data-table/data-table.component';
+import { EditFormComponent } from '@components/product-pages/edit-form/edit-form.component';
+import { AppMaterialModule } from './app.material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '@app/app-routing.module';
+
+@NgModule({
+  declarations: [
+    DataTableComponent,
+    EditFormComponent,
+    AddFormComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    UsersTableComponent,
+    UsersEditComponent,
+    ProductInfoComponent,
+    EditLocationComponent,
+    AddLocationComponent,
+  ],
+  imports: [
+    CommonModule,
+    AppMaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule
+  ]
+})
+export class CompomponentsModule { }
--- wms-praktyki-yosi-frontend/wms-frontend/src/app/app.module.ts
+++ wms-praktyki-yosi-frontend/wms-frontend/src/app/app.module.ts
@@ -1,45 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppMaterialModule } from "./app.material-module";
+import { AppMaterialModule } from "./Modules/app.material-module";
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTableComponent } from './Components/product-pages/data-table/data-table.component';
-import { EditFormComponent } from './Components/product-pages/edit-form/edit-form.component';
-import { ReactiveFormsModule } from "@angular/forms";
-import { AddFormComponent } from './Components/product-pages/add-form/add-form.component'
-import { LoginFormComponent } from './Components/user-pages/login-form/login-form.component';
-import { RegisterFormComponent } from './Components/user-pages/register-form/register-form.component';
-import { UsersTableComponent } from './Components/user-pages/users-table/users-table.component';
-import { UsersEditComponent } from './Components/user-pages/users-edit/users-edit.component';
 import { NavigationComponent } from './Components/navigation/navigation.component';
-import { ProductInfoComponent } from './Components/product-pages/product-info/product-info.component';
-import { EditLocationComponent } from './Components/location-pages/edit-location/edit-location.component';
-import { AddLocationComponent } from './Components/location-pages/add-location/add-location.component';
+import { CompomponentsModule } from './Modules/compomponents.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataTableComponent,
-    EditFormComponent,
-    AddFormComponent,
-    LoginFormComponent,
-    RegisterFormComponent,
-    UsersTableComponent,
-    UsersEditComponent,
-    NavigationComponent,
-    ProductInfoComponent,
-    EditLocationComponent,
-    AddLocationComponent,
+    NavigationComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    CompomponentsModule,
     AppMaterialModule,
-    ReactiveFormsModule,
     FormsModule
   ],
   providers: [],
